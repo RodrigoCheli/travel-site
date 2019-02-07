ac39e33caa641fffd8619b0258e3aae12032ea82
Rodrigo <rodrigoecherry@gmail.com>
2019-02-07 20:49:02 -0200

    terminado esmarnescer

---
+++ app/assets/scripts/modules/Person.js
@@ -0,0 +1,21 @@
+/*function Person(fullname, favColor){
+  this.name = fullname;
+  this.favoriteColor = favColor;
+  this.greet = function(){
+    console.log("Hello, my name is " + this.name + "and may fav color is " + this.favColor + ".");
+  }
+}
+
+class Person{
+  constructor(fullName, favColor){
+    this.name = fullName;
+    this.favColor = favColor;
+  }
+
+  greet(){
+    console.log("Hi there, my name is " + this.name + "and may fav color is " + this.favColor + ".");
+  }
+}
+
+export default Person;
+*/
