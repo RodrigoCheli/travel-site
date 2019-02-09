50a7beb21177c73e243268a23cc51029600ebf65
Rodrigo <rodrigoecherry@gmail.com>
2019-02-09 12:03:52 -0200

    complete lazyy loadin of images

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,17 @@
-/*var path = require('path');
-
 module.exports = {
-  entry: "./app/assets/scripts/App.js",
-  output: {
-    path: path.resolve(__dirname, "./app/temp/scripts"),
-    filename: "App.js"
-  }
-}*/
-
-const path = require('path');
-
-module.exports = {
-
-  entry: "./app/assets/scripts/App.js",
-
-  output: {
-
-    path: path.resolve(__dirname, "./app/temp/scripts"),
-
-    filename: "App.js"
-
+  entry: {
+    App: "./app/assets/scripts/App.js",
+    Vendor: "./app/assets/scripts/Vendor.js"
   },
-  module:{
-    loaders:[
+  output: {
+    path: "./app/temp/scripts",
+    filename: "[name].js"
+  },
+  module: {
+    loaders: [
       {
-        loader: 'babel-loader',
-        query:{
+        loader: 'babel',
+        query: {
           presets: ['es2015']
         },
         test: /\.js$/,
@@ -33,5 +19,4 @@ module.exports = {
       }
     ]
   }
-
 }
